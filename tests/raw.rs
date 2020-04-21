11790d6bae9d9fd5c02cd44e9970a1331e212da0
CAD97 <cad97@cad97.com>
2020-04-20 22:44:59 -0400

    fix stable/reference/comments test's indent

--- tests/raw.rs
+++ tests/raw.rs
@@ -42,61 +42,61 @@ fn examples_in_informal_reference() {
 
 pub mod outer_module {
 
-//!  - Inner line doc
-//!! - Still an inner line doc (but with a bang at the beginning)
+    //!  - Inner line doc
+    //!! - Still an inner line doc (but with a bang at the beginning)
 
-/*!  - Inner block doc */
-/*!! - Still an inner block doc (but with a bang at the beginning) */
+    /*!  - Inner block doc */
+    /*!! - Still an inner block doc (but with a bang at the beginning) */
 
-//   - Only a comment
-///  - Outer line doc (exactly 3 slashes)
-//// - Only a comment
+    //   - Only a comment
+    ///  - Outer line doc (exactly 3 slashes)
+    //// - Only a comment
 
-/*   - Only a comment */
-/**  - Outer block doc (exactly) 2 asterisks */
-/*** - Only a comment */
+    /*   - Only a comment */
+    /**  - Outer block doc (exactly) 2 asterisks */
+    /*** - Only a comment */
 
-pub mod inner_module {}
+    pub mod inner_module {}
 
-pub mod nested_comments {
-    /* In Rust /* we can /* nest comments */ */ */
+    pub mod nested_comments {
+        /* In Rust /* we can /* nest comments */ */ */
 
-    // All three types of block comments can contain or be nested inside
-    // any other type:
+        // All three types of block comments can contain or be nested inside
+        // any other type:
 
-    /*   /* */  /** */  /*! */  */
-    /*!  /* */  /** */  /*! */  */
-    /**  /* */  /** */  /*! */  */
-    pub mod dummy_item {}
-}
+        /*   /* */  /** */  /*! */  */
+        /*!  /* */  /** */  /*! */  */
+        /**  /* */  /** */  /*! */  */
+        pub mod dummy_item {}
+    }
 
-pub mod degenerate_cases {
-    // empty inner line doc
-    //!
+    pub mod degenerate_cases {
+        // empty inner line doc
+        //!
 
-    // empty inner block doc
-    /*!*/
+        // empty inner block doc
+        /*!*/
 
-    // empty line comment
-    //
+        // empty line comment
+        //
 
-    // empty outer line doc
-    ///
+        // empty outer line doc
+        ///
 
-    // empty block comment
-    /**/
+        // empty block comment
+        /**/
 
-    pub mod dummy_item {}
+        pub mod dummy_item {}
 
-    // empty 2-asterisk block isn't a doc block, it is a block comment
-    /***/
+        // empty 2-asterisk block isn't a doc block, it is a block comment
+        /***/
 
-}
+    }
 
-/* The next one isn't allowed because outer doc comments
-    require an item that will receive the doc */
+    /* The next one isn't allowed because outer doc comments
+        require an item that will receive the doc */
 
-/// Where is my item?
+    /// Where is my item?
 }",
             // <https://doc.rust-lang.org/stable/reference/whitespace.html>
             "\u{0009}", "\u{000A}", "\u{000B}", "\u{000C}", "\u{000D}",
